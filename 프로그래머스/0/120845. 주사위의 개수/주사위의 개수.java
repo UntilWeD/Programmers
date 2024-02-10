e91560861b791b164beeef3675c22570e3e3d11d
UntilWeD <109950597+UntilWeD@users.noreply.github.com>
2024-02-10 16:37:49 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 1;
+        
+        for(int i=0; i<box.length; i++){
+            answer *= box[i]/n;
+        }
+        
+
+
+        
+        return answer;
+    }
+}
