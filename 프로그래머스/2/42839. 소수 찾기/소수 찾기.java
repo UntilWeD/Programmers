10bc5e97c109ae01824e940b498ba33453ad5807
jwk7539 <muojeso90@gmail.com>
2025-02-19 22:35:45 +0900

    [level 2] Title: 소수 찾기, Time: 12.61 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,45 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(String numbers) {
-        int answer = 0;
         String[] nums = numbers.split("");
-        boolean[] visited = new boolean[9999999];
-        List<String> list = new ArrayList<>();
-        for(String num : nums)
-            list.add(num);
-        
-        return search(list, "", visited);
+        boolean[] used = new boolean[nums.length];
+        Set<Integer> visited = new HashSet<>(); // 방문 여부 체크 및 중복 방지
+
+        search(nums, used, "", visited);
+        return visited.size();
     }
-    
-    public int search(List<String> list, String num, boolean[] visited){
-        int sum = 0;
-        if(!num.equals("") && isPrime(Integer.parseInt(num)) && !visited[Integer.parseInt(num)]){
-            visited[Integer.parseInt(num)] = true;
-            sum += 1;
+
+    public void search(String[] nums, boolean[] used, String current, Set<Integer> visited) {
+        if (!current.equals("")) {
+            int number = Integer.parseInt(current);
+            if (!visited.contains(number) && isPrime(number)) {
+                visited.add(number);
+            }
         }
-            
-        for(int i=0; i<list.size(); i++){
-            List<String> copiedList = new ArrayList<>(list);
-            String temp = list.get(i);
-            copiedList.remove(i);
-            sum += search(copiedList, num+temp, visited);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                search(nums, used, current + nums[i], visited);
+                used[i] = false; // 백트래킹
+            }
         }
-        
-        
-        return sum;
     }
-    
-    public boolean isPrime(int num){
-        if(num <= 1) return false;
-        if(num <= 3) return true;
-        if(num % 2 == 0 || num % 3 == 0) return false;
-        
-        for(int i=5; i <= (int)Math.sqrt(num); i++){
-            if(num % i == 0)
-                return false;
+
+    public boolean isPrime(int num) {
+        if (num <= 1) return false;
+        if (num == 2 || num == 3) return true; // 2와 3은 소수
+        if (num % 2 == 0 || num % 3 == 0) return false; // 2와 3으로 나누어떨어지면 소수 아님
+
+        // 6의 배수 ± 1 확인
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) return false;
         }
-        
         return true;
     }
-    
 }
