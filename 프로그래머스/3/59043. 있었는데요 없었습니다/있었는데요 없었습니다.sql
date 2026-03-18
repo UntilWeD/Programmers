7fb8c4964cca6a777b93b19237a724f406e974cb
jwk7539 <muojeso90@gmail.com>
2026-03-18 11:16:50 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT      T1.ANIMAL_ID
+           ,T1.NAME
+  FROM      ANIMAL_INS  T1
+           ,ANIMAL_OUTS T2
+ WHERE      T1.ANIMAL_ID    =   T2.ANIMAL_ID
+ AND        T1.ANIMAL_TYPE  =   T2.ANIMAL_TYPE
+ AND        T1.DATETIME     >   T2.DATETIME
+ORDER   BY  T1.DATETIME ASC
+ 
